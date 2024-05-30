71afb6e164b51cab6775c3c0ec8ec6a58b84b167
Sourav <sourvakunofficial000@gmail.com>
2024-05-30 22:21:45 +0530

    removed formidable compression body-parser

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -6,15 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("./Configurations/dotenv");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
-const compression_1 = __importDefault(require("compression"));
-// import formidable from "formidable";
 const Route_1 = __importDefault(require("./Routes/Route"));
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
-app.use((0, body_parser_1.default)({ extended: true }));
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json());
 app.use((0, morgan_1.default)("combined"));
 app.use(helmet_1.default.contentSecurityPolicy({
     directives: {
@@ -26,7 +24,5 @@ app.use(helmet_1.default.contentSecurityPolicy({
     includeSubDomains: true,
     preload: true,
 }), helmet_1.default.xXssProtection(), helmet_1.default.frameguard({ action: "sameorigin" }), helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use((0, compression_1.default)());
-// app.use(formidable({}));
 app.use("/", Route_1.default);
 app.listen(dotenv_1.PORT_NO, () => console.info("Server running on port:", dotenv_1.PORT_NO));
--- Server/source/index.ts
+++ Server/source/index.ts
@@ -1,16 +1,14 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { PORT_NO } from "./Configurations/dotenv";
 import cookie_parser from "cookie-parser";
-import body_parser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
-import compression from "compression";
-// import formidable from "formidable";
 import Router from "./Routes/Route";
 
 const app = express();
 app.use(cookie_parser());
-app.use(body_parser({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(morgan("combined"));
 app.use(
   helmet.contentSecurityPolicy({
@@ -28,8 +26,6 @@ app.use(
   helmet.frameguard({ action: "sameorigin" }),
   helmet.crossOriginResourcePolicy({ policy: "cross-origin" })
 );
-app.use(compression());
-// app.use(formidable({}));
 
 app.use("/", Router);
 
